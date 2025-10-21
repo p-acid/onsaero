daef847b16456f8c524b80e8a4df2d1be5cd69a9
p-acid <qkrtkstjd01@gmail.com>
2025-10-21 14:43:46 +0900

    feat: 로그인 유지 추가

--- apps/web/src/app/loader/protected-loader.ts
+++ apps/web/src/app/loader/protected-loader.ts
@@ -1,11 +1,21 @@
-import { PAGE_ROUTES } from '@onsaero/shared'
+import { PAGE_ROUTES, supabase } from '@onsaero/shared'
 import { redirect } from 'react-router'
 import { webAuthStore } from '@/shared/store'
 
 export const protectedLoader = async () => {
-  const { session } = webAuthStore.getState()
+  const { session, updateAuth } = webAuthStore.getState()
 
   if (!session) {
+    const { data } = await supabase.auth.getSession()
+
+    if (data.session?.user) {
+      updateAuth({
+        user: data.session?.user,
+        session: data.session,
+      })
+      return null
+    }
+
     throw redirect(PAGE_ROUTES.SIGN_IN)
   }
 
